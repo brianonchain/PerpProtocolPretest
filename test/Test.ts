c962cb6baefe74bd86f257d3a4767c5cf3107789
brianonchain <jonwayhuang@gmail.com>
2024-06-14 13:52:25 +0800

    correct zeros

--- test/Test.ts
+++ test/Test.ts
@@ -6,8 +6,8 @@ describe("LeveargedAMM", function () {
   let leveragedAMMFactory: LeveragedAMM__factory;
   let leveragedAMM: LeveragedAMM;
   let reserves: { eth: bigint; usdc: bigint } = {
-    eth: BigInt(1000),
-    usdc: BigInt(3500000),
+    eth: BigInt(1000 * 10 ** 18),
+    usdc: BigInt(3500000 * 10 ** 18),
   };
   const k = reserves.eth * reserves.usdc;
 
@@ -19,17 +19,24 @@ describe("LeveargedAMM", function () {
   });
 
   it("Should get expected ETH amount if user wants to long 1000 USDC worth of ETH", async function () {
-    const usdcAmount = ethers.parseUnits("1000", 6);
+    let usdcAmount = ethers.parseUnits("1000", 6);
+    // actual
     const ethAmount = await leveragedAMM.getEthAmount(1, usdcAmount);
+    // expected
+    usdcAmount = usdcAmount * BigInt(10 ** 12);
     const expectedEthAmount = reserves.eth - k / (reserves.usdc + usdcAmount);
+
     assert.equal(ethAmount, expectedEthAmount);
   });
 
   it("Should get expected ETH amount if user wants to short 1000 USDC worth of ETH", async function () {
-    const usdcAmount = ethers.parseUnits("1000", 6);
+    let usdcAmount = ethers.parseUnits("1000", 6);
+    // actual
     const ethAmount = await leveragedAMM.getEthAmount(-1, usdcAmount);
+    // expected
+    usdcAmount = usdcAmount * BigInt(10 ** 12);
     const expectedEthAmount = reserves.eth - k / (reserves.usdc - usdcAmount);
-    assert.equal(ethAmount, expectedEthAmount);
+    assert.equal(ethAmount, expectedEthAmount); // expect negative eth
   });
 
   it("Should get expected USDC amount if user wants to long 1 ETH", async function () {
